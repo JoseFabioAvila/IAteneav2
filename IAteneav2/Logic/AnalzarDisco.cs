2a5e51b7d8609424b32056307c48873f36c43bc9
JoseFabioAvila <fabioavila-16@hotmail.com>
2016-03-29 00:56:16 -0600

    treeView

--- IAteneav2/Logic/AnalzarDisco.cs
+++ IAteneav2/Logic/AnalzarDisco.cs
@@ -46,5 +46,33 @@ namespace IAteneav2.Logic
                 return body.InnerText;
             }
         }
+
+        public string GetFilesFromDirectory(string DirPath)
+        {
+            string x = "inicio \n";
+            try
+            {
+                DirectoryInfo Dir = new DirectoryInfo(DirPath);
+                DirectoryInfo[] DirList = Dir.GetDirectories();
+                FileInfo[] FileList = Dir.GetFiles("*.*", SearchOption.AllDirectories);
+
+                foreach (DirectoryInfo FI in DirList)
+                {
+                    x += FI.FullName + "\n";
+                }
+                x += "\n\n\n";
+                
+                foreach (FileInfo FI in FileList)
+                {
+                    x += FI.FullName + "\n";
+                }
+
+                return x;
+            }
+            catch (Exception ex)
+            {
+                return x;
+            }
+        }
     }
 }
